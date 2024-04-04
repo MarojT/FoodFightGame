94530570ee9523566e9c3fe7e3687e2fee099a28
MarojT <celta0992@hotmail.com>
2024-04-04 08:01:56 +0700

    print gamover if aby animal pass lowerbound.

--- Assets/Scripts/DestroyOutbound.cs
+++ Assets/Scripts/DestroyOutbound.cs
@@ -16,7 +16,10 @@ public class DestroyOutbound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > upperBound || transform.position.z < lowerBound){
+        if (transform.position.z > upperBound){
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBound){
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
