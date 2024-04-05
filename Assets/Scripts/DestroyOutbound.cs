7e0b5de1ac6a0644e8bfe2d06041ae0f5fd15d7a
Maroj Thangthong <maroj.t@ku.th>
2024-04-05 14:35:19 +0700

    Implement health system.

--- Assets/Scripts/DestroyOutbound.cs
+++ Assets/Scripts/DestroyOutbound.cs
@@ -7,10 +7,11 @@ public class DestroyOutbound : MonoBehaviour
 {
     public float upperBound = 50f;
     public float lowerBound = -10;
+    public static Health pHealth = new Health();
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,8 +20,13 @@ public class DestroyOutbound : MonoBehaviour
         if (transform.position.z > upperBound){
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound){
-            Debug.Log("Game Over!");
+            Debug.Log(pHealth.hPoint);
+            pHealth.hPoint -=1;
+            Debug.Log(pHealth.hPoint);
             Destroy(gameObject);
+            if (pHealth.hPoint <= 0){
+                Debug.Log("Game Over!");
+            }
         }
 
     }
